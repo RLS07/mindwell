d024ca9793035b760f9ef980cd3e700d1c845b08
Rojan Lal Shrestha <lalpower77@gmail.com>
2023-05-01 09:28:38 +0545

    added all files from mac

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog </title>
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <h1>hello HELLo</h1>
+</body>
+</html>
